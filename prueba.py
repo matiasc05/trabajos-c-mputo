bdc99102331e53c79020ebbda1cdebc544213da5
Aylin Salazar Delgado <aylinsalazard0901@gmail.com>
2024-08-20 16:36:16 -0600

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,8 @@
+#Aylin Salazar Delgado y matías lizano 
+import numpy 
+import cv2
+import face_recognition
+
+print (numpy.__version__)
+print (cv2.__version__)
+print (face_recognition.__version__)
